2ba163c4880acf58bac8eb76072ce168722b8449
jackspadew <201367434+jackspadew@users.noreply.github.com>
2024-12-29 10:21:26 +0900

    add: Created new class "DressableCryptographicConverter" and its Tests.

--- LibEncryptedDriveScripts.Tests/UnitTest1.cs
+++ LibEncryptedDriveScripts.Tests/UnitTest1.cs
@@ -2,5 +2,48 @@ namespace LibEncryptedDriveScripts.Tests;
 
 using Xunit;
 using LibEncryptedDriveScripts;
+using System.Security.Cryptography;
 
+public class DressableCryptographicConverterTests
+{
+    private DressableCryptographicConverter converter;
+    private HashAlgorithm hashAlgo_SHA2_256 = SHA256.Create();
+    private SymmetricAlgorithm encryptAlgo_DES = DES.Create();
+    public DressableCryptographicConverterTests()
+    {
+        converter = new DressableCryptographicConverter();
+    }
 
+    [Theory]
+    [InlineData(new byte[] {0,0,0,0})]
+    [InlineData(new byte[] {0,1,2,3})]
+    [InlineData(new byte[] {0,1,2,3,4,5})]
+    public void EncryptThenDecrypt_InputAndDecryptedBytesIsEqual(byte[] input)
+    {
+        byte[] encrypted = converter.Encrypt(input);
+        byte[] decrypted = converter.Decrypt(encrypted);
+        Assert.Equal(input.Length, decrypted.Length);
+        Assert.Equal(input, decrypted);
+    }
+
+    [Fact]
+    public void UsingSHA256HashAlgorithm_InputAndDecryptedIsEqual()
+    {
+        DressableCryptographicConverter converter = new DressableCryptographicConverter(hashAlgo_SHA2_256);
+        byte[] input = new byte[] {0,1,2,3};
+        byte[] encrypted = converter.Encrypt(input);
+        byte[] decrypted = converter.Decrypt(encrypted);
+        Assert.Equal(input.Length, decrypted.Length);
+        Assert.Equal(input, decrypted);
+    }
+    [Fact]
+    public void UsingSHA384HashAlgorithm_InputAndDecryptedIsEqual()
+    {
+        DressableCryptographicConverter converter = new DressableCryptographicConverter(encryptAlgo_DES);
+        byte[] input = new byte[] {0,1,2,3};
+        byte[] encrypted = converter.Encrypt(input);
+        byte[] decrypted = converter.Decrypt(encrypted);
+        Assert.Equal(input.Length, decrypted.Length);
+        Assert.Equal(input, decrypted);
+    }
+}
--- LibEncryptedDriveScripts/Class1.cs
+++ LibEncryptedDriveScripts/Class1.cs
@@ -1,6 +1,108 @@
 ﻿namespace LibEncryptedDriveScripts;
 
-public class Class1
-{
+using System.Security.Cryptography;
+using System.Text;
 
+public class DressableCryptographicConverter
+{
+    private byte[] _secretKey = new byte[32];
+    private byte[] _iv = new byte[16];
+
+    private HashAlgorithm hashAlgorithm = SHA512.Create();
+    private SymmetricAlgorithm encryptAlgorithm = Aes.Create();
+
+    public int SizeOfSecretKey {
+        get {
+            int maxKeySizeInBits = this.encryptAlgorithm.LegalKeySizes.Max(keySize => keySize.MaxSize);
+            return maxKeySizeInBits / 8;
+        }
+    }
+    public int SizeOfIV {
+        get {
+            return this.encryptAlgorithm.IV.Length;
+        }
+    }
+    public DressableCryptographicConverter()
+    {
+        SetLegalSizeKeys();
+    }
+    public DressableCryptographicConverter(HashAlgorithm inputHashAlgorithm)
+    {
+        hashAlgorithm = inputHashAlgorithm;
+        SetLegalSizeKeys();
+    }
+    public DressableCryptographicConverter(SymmetricAlgorithm inputEncrptAlgorithm)
+    {
+        encryptAlgorithm = inputEncrptAlgorithm;
+        SetLegalSizeKeys();
+    }
+    public DressableCryptographicConverter(HashAlgorithm inputHashAlgorithm, SymmetricAlgorithm inputEncrptAlgorithm) : this()
+    {
+        hashAlgorithm = inputHashAlgorithm;
+        encryptAlgorithm = inputEncrptAlgorithm;
+        SetLegalSizeKeys();
+    }
+    private void SetLegalSizeKeys()
+    {
+        SecretKey = new byte[SizeOfSecretKey];
+        IV = new byte[SizeOfIV];
+    }
+    public byte[] Encrypt(byte[] inputBytes)
+    {
+        byte[] encrypted;
+        this.encryptAlgorithm.BlockSize = this.encryptAlgorithm.LegalBlockSizes.Max(keySize => keySize.MaxSize);
+        ICryptoTransform encryptor = this.encryptAlgorithm.CreateEncryptor(this.SecretKey, this.IV);
+        using (MemoryStream msEncrypt = new MemoryStream())
+        {
+            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            {
+                csEncrypt.Write(inputBytes);
+                csEncrypt.FlushFinalBlock();
+            }
+            encrypted = msEncrypt.ToArray();
+        }
+        return encrypted;
+    }
+    public byte[] Encrypt(string inputString)
+    {
+        byte[] inputBytes = Encoding.UTF8.GetBytes(inputString);
+        return this.Encrypt(inputBytes);
+    }
+    public byte[] Decrypt(byte[] encryptedBytes)
+    {
+        byte[] decrypted;
+        ICryptoTransform decryptor = this.encryptAlgorithm.CreateDecryptor(this.SecretKey, this.IV);
+        using (MemoryStream msDecrypt = new MemoryStream(encryptedBytes))
+        {
+            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            {
+                using (MemoryStream decryptedStream = new MemoryStream())
+                {
+                    csDecrypt.CopyTo(decryptedStream);
+                    decrypted = decryptedStream.ToArray();
+                }
+            }
+        }
+        return decrypted;
+    }
+    private byte[] SecretKey {
+        set {
+            byte[] tmpBytes = hashAlgorithm.ComputeHash(value);
+            Array.Resize( ref tmpBytes, SizeOfSecretKey);
+            this._secretKey = tmpBytes;
+        }
+        get {
+            return this._secretKey;
+        }
+    }
+    private byte[] IV {
+        set {
+            byte[] tmpBytes = hashAlgorithm.ComputeHash(value);
+            Array.Resize( ref tmpBytes, SizeOfIV);
+            this._iv = tmpBytes;
+        }
+        get {
+            return this._iv;
+        }
+    }
 }
