da795fb71109226dab6a2bb6e9b59ed50d53ce7b
jackspadew <201367434+jackspadew@users.noreply.github.com>
2024-12-30 11:56:25 +0900

    add: Added interface "ISymmetricAlgorithmAdapter".

---
+++ LibEncryptedDriveScripts/ISymmetricAlgorithmAdapter.cs
@@ -0,0 +1,9 @@
+namespace LibEncryptedDriveScripts;
+
+public interface ISymmetricAlgorithmAdapter
+{
+    int LegalIVSize { get; }
+    int LegalKeySize { get; }
+    Stream CreateEncryptStream(Stream inputStream, byte[] key, byte[] iv);
+    Stream CreateDecryptStream(Stream inputStream, byte[] key, byte[] iv);
+}
