456654c154db9e9a9e68da9091ee9867a24d65c9
jackspadew <201367434+jackspadew@users.noreply.github.com>
2024-12-30 16:34:47 +0900

    add: Created the "AES" class that adapts AES engine of BouncyCastle.

---
+++ LibEncryptedDriveScripts.Tests/Adapter_BCSymmetricAlgorithmTests.cs
@@ -0,0 +1,28 @@
+namespace LibEncryptedDriveScripts.Tests;
+
+using Xunit;
+using LibEncryptedDriveScripts.BouncyCastleAdapter;
+
+public class BCSymmetricAlgorithmTests
+{
+    public static IEnumerable<object[]> EncryptAlgorithObjects()
+    {
+        yield return new object[] { new BouncyCastleAdapter.AES() };
+    }
+
+    [Theory]
+    [MemberData(nameof(EncryptAlgorithObjects))]
+    public void EncryptThenDecrypt_BeforeAndAfterIsEqual(ISymmetricAlgorithmAdapter encryptAlgo)
+    {
+        byte[] inputBytes = new byte[] {0,1,2,3};
+        byte[] iv = new byte[encryptAlgo.LegalIVSize];
+        byte[] key = new byte[encryptAlgo.LegalKeySize];
+        MemoryStream inputStream = new MemoryStream(inputBytes);
+        MemoryStream outputStream = new MemoryStream();
+        var EncryptedStream = encryptAlgo.CreateEncryptStream(inputStream,key,iv);
+        var DecryptedStream = encryptAlgo.CreateDecryptStream(EncryptedStream,key,iv);
+        DecryptedStream.CopyTo(outputStream);
+        byte[] outputBytes = outputStream.ToArray();
+        Assert.Equal(outputBytes, inputBytes);
+    }
+}
---
+++ LibEncryptedDriveScripts/Adapter_BCSymmetricAlgorithm.cs
@@ -0,0 +1,32 @@
+namespace LibEncryptedDriveScripts.BouncyCastleAdapter;
+
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.IO;
+using Org.BouncyCastle.Crypto.Paddings;
+
+public class AES : ISymmetricAlgorithmAdapter
+{
+    public int LegalIVSize {
+        get { return (new AesEngine()).GetBlockSize(); }
+    }
+    public int LegalKeySize { get {return 16;}}
+
+    public Stream CreateDecryptStream(Stream inputStream, byte[] key, byte[] iv)
+    {
+        return CreateStream(false, inputStream, key, iv);
+    }
+
+    public Stream CreateEncryptStream(Stream inputStream, byte[] key, byte[] iv)
+    {
+        return CreateStream(true, inputStream, key, iv);
+    }
+    private Stream CreateStream(bool forEncryption, Stream inputStream, byte[] key, byte[] iv)
+    {
+        var cipher = new PaddedBufferedBlockCipher(new CbcBlockCipher(new AesEngine()));
+        cipher.Init(forEncryption, new ParametersWithIV(new KeyParameter(key), iv));
+
+        return new CipherStream(inputStream, cipher, cipher);
+    }
+}
