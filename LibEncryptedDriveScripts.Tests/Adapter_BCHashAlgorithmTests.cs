e3b4dad43c88915d959adfa57785aee4ee17a970
jackspadew <201367434+jackspadew@users.noreply.github.com>
2024-12-30 13:31:21 +0900

    add: Created the "SHA3" class that adapts SHA3 hash algorithm of BouncyCastle.

---
+++ LibEncryptedDriveScripts.Tests/Adapter_BCHashAlgorithmTests.cs
@@ -0,0 +1,34 @@
+namespace LibEncryptedDriveScripts.Tests;
+
+using Xunit;
+using LibEncryptedDriveScripts.Adapter.BouncyCastle;
+
+public class Adapter_BCHashAlgorithmTests
+{
+    [Fact]
+    public void ComputeHashBySHA3_HashLengthIs512bits()
+    {
+        IHashAlgorithmAdapter hashAlgo = new SHA3();
+        byte[] hash = hashAlgo.ComputeHash(new byte[0], new byte[0]);
+        Assert.Equal((512/8), hash.Length);
+    }
+
+    [Theory]
+    [InlineData(512)]
+    [InlineData(224)]
+    [InlineData(256)]
+    [InlineData(384)]
+    public void ComputeHashBySHA3_HashLengthIsGivenValue(int value)
+    {
+        IHashAlgorithmAdapter hashAlgo = new SHA3(value);
+        byte[] hash = hashAlgo.ComputeHash(new byte[0], new byte[0]);
+        Assert.Equal((value/8), hash.Length);
+    }
+
+    [Theory]
+    [InlineData(0)][InlineData(123)][InlineData(1024)]
+    public void InitSHA3WithIllegalValue_WillThrow(int value)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new SHA3(value));
+    }
+}
