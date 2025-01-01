bfe04abdc4c3f5da23d3e2d17eb5e61a321ed138
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-01 09:45:47 +0900

    refactor: Updated "SymmetricAlgorithmAdapter.SystemCryptography.AES".
    Based on refactoring of ISymmetricAlgorithmAdapter.
    The tests passed.

--- LibEncryptedDriveScripts/Adapter_CryptographySymmetricAlgorithm.cs
+++ LibEncryptedDriveScripts/Adapter_CryptographySymmetricAlgorithm.cs
@@ -5,42 +5,31 @@ using LibEncryptedDriveScripts.SymmetricAlgorithmAdapter;
 using System.IO;
 using System.Security.Cryptography;
 
-public class AES : ISymmetricAlgorithmAdapter
+public class AES : SymmetricAlgorithmAdapterBase,ISymmetricAlgorithmAdapter
 {
-    public int LegalIVSize {
+    public override int LegalIVSize {
         get { return 16; }
     }
-    public int LegalKeySize { get {return 32;}}
+    public override int LegalKeySize { get {return 32;}}
 
-    public Stream CreateDecryptStream(Stream inputStream, byte[] key, byte[] iv)
-    {
-        return CreateAesDecryptedStream(inputStream, key, iv);
-    }
-
-    public Stream CreateEncryptStream(Stream inputStream, byte[] key, byte[] iv)
-    {
-        return CreateAesEncryptedStream(inputStream, key, iv);
-    }
-    private Stream CreateAesEncryptedStream(Stream inputStream, byte[] key, byte[] iv)
+    public override void Encrypt(Stream inputStream, Stream outputStream, byte[] key, byte[] iv)
     {
         System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create();
         aes.Key = key;
         aes.IV = iv;
-        Stream outputStream = new MemoryStream();
-        using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
-        using (var cryptoStream = new CryptoStream(outputStream, encryptor, CryptoStreamMode.Write, true))
+        using (var encryptStream = new CryptoStream(outputStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
         {
-            inputStream.CopyTo(cryptoStream);
+            inputStream.CopyTo(encryptStream);
         }
-        outputStream.Position = 0;
-        return outputStream;
     }
-    private Stream CreateAesDecryptedStream(Stream inputStream, byte[] key, byte[] iv)
+    public override void Decrypt(Stream inputStream, Stream outputStream, byte[] key, byte[] iv)
     {
         System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create();
         aes.Key = key;
         aes.IV = iv;
-        Stream decryptStream = new CryptoStream(inputStream, aes.CreateDecryptor(aes.Key,aes.IV), CryptoStreamMode.Read, true);
-        return decryptStream;
+        using (var decryptStream = new CryptoStream(inputStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+        {
+            decryptStream.CopyTo(outputStream);
+        }
     }
 }
