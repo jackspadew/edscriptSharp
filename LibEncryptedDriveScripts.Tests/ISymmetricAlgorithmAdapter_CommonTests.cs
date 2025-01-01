04274102f34b75314434c981bb0dc7733cec1860
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-01 21:13:58 +0900

    add: Added new test "EncryptWithStream_BeforeAndAfterIsEqual".

--- LibEncryptedDriveScripts.Tests/ISymmetricAlgorithmAdapter_CommonTests.cs
+++ LibEncryptedDriveScripts.Tests/ISymmetricAlgorithmAdapter_CommonTests.cs
@@ -37,4 +37,22 @@ public class BCSymmetricAlgorithmTests
         byte[] decryptedBytes = encryptAlgo.DecryptBytes(encryptedBytes,key,iv);
         Assert.Equal(inputBytes, decryptedBytes);
     }
+
+    [Theory]
+    [MemberData(nameof(EncryptAlgorithObjects))]
+    public void EncryptWithStream_BeforeAndAfterIsEqual(ISymmetricAlgorithmAdapter encryptAlgo, string className)
+    {
+        byte[] inputBytes = new byte[] {0,1,2,3};
+        byte[] iv = new byte[encryptAlgo.LegalIVSize];
+        byte[] key = new byte[encryptAlgo.LegalKeySize];
+        MemoryStream inputEncryptStream = new MemoryStream(inputBytes);
+        MemoryStream encryptedStream = new MemoryStream();
+        encryptAlgo.Encrypt(inputEncryptStream, encryptedStream, key, iv);
+        byte[] encryptedBytes = encryptedStream.ToArray();
+        MemoryStream inputDecryptStream = new MemoryStream(encryptedBytes);
+        MemoryStream outputStream = new MemoryStream();
+        encryptAlgo.Decrypt(inputDecryptStream, outputStream, key, iv);
+        byte[] decryptedBytes = outputStream.ToArray();
+        Assert.Equal(inputBytes, decryptedBytes);
+    }
 }
