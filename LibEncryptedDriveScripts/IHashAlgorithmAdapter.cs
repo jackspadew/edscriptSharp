9f38a5923c2093b390c7e69035d7d2aa5ae52656
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-02 13:01:50 +0900

    add: Added overload methods of "ComputeHash".

--- LibEncryptedDriveScripts/IHashAlgorithmAdapter.cs
+++ LibEncryptedDriveScripts/IHashAlgorithmAdapter.cs
@@ -2,5 +2,8 @@ namespace LibEncryptedDriveScripts;
 
 public interface IHashAlgorithmAdapter
 {
+    byte[] ComputeHash(byte[] inputBytes);
+    byte[] ComputeHash(Stream inputStream);
     byte[] ComputeHash(byte[] inputBytes, byte[] solt);
+    byte[] ComputeHash(Stream inputStream, byte[] solt);
 }
