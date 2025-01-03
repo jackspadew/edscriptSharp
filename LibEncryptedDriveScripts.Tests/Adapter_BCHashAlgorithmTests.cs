836951171c5943d6f8f50f1e8d40f800cad40a9c
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-03 11:49:28 +0900

    test: cleanup "Adapter_BCHashAlgorithmTests".
    The same test as the common test for interface IHashAlgorithmAdapter was deleted.
    As a result, deleted class properties that were no longer needed.

--- LibEncryptedDriveScripts.Tests/Adapter_BCHashAlgorithmTests.cs
+++ LibEncryptedDriveScripts.Tests/Adapter_BCHashAlgorithmTests.cs
@@ -5,11 +5,6 @@ using LibEncryptedDriveScripts.HashAlgorithmAdapter;
 
 public class Adapter_BCHashAlgorithmTests
 {
-    public static IEnumerable<object[]> HashAlgorithObjects()
-    {
-        yield return new object[] { new HashAlgorithmAdapter.BouncyCastle.SHA3() };
-    }
-
     [Fact]
     public void ComputeHashBySHA3_HashLengthIs512bits()
     {
@@ -35,17 +30,5 @@ public class Adapter_BCHashAlgorithmTests
     public void InitSHA3WithIllegalValue_WillThrow(int value)
     {
         Assert.Throws<ArgumentOutOfRangeException>(() => new HashAlgorithmAdapter.BouncyCastle.SHA3(value));
-    }
-
-    [Theory]
-    [MemberData(nameof(HashAlgorithObjects))]
-    public void ComputeHashAnotherSolt_WillOutAnotherHash(IHashAlgorithmAdapter hashAlgo)
-    {
-        byte[] data = {0,1,2,3,4,5,6,7};
-        byte[] soltOne = {0};
-        byte[] soltTwo = {1};
-        byte[] hashSoltOne = hashAlgo.ComputeHash(data, soltOne);
-        byte[] hashSoltTwo = hashAlgo.ComputeHash(data, soltTwo);
-        Assert.NotEqual(hashSoltOne, hashSoltTwo);
     }
 }
