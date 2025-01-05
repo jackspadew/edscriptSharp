f3abfce59a98d2ef1cdc370b6768ce2930e82e1b
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-05 17:26:17 +0900

    add: Created files for "IDatabaseOperator" and "DatabaseOperatorBase".
    Added interface "IDatabaseOperator".
    Added abstract class "DatabaseOperatorBase".

---
+++ LibEncryptedDriveScripts/database/DatabaseOperator.cs
@@ -0,0 +1,8 @@
+namespace LibEncryptedDriveScripts.Database;
+
+using Microsoft.Data.Sqlite;
+
+public abstract class DatabaseOperatorBase : IDatabaseOperator
+{
+
+}
---
+++ LibEncryptedDriveScripts/database/IDatabaseOperator.cs
@@ -0,0 +1,6 @@
+namespace LibEncryptedDriveScripts.Database;
+
+public interface IDatabaseOperator
+{
+
+}
