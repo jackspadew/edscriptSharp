9675e78bb868618921c3012d8de4fcbac1d57b40
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-06 19:42:45 +0900

    add: [NOT TESTED] Added some members to "DatabaseOperatorBase".
    Not implemented.

--- LibEncryptedDriveScripts/database/DatabaseOperator.cs
+++ LibEncryptedDriveScripts/database/DatabaseOperator.cs
@@ -1,8 +1,43 @@
 namespace LibEncryptedDriveScripts.Database;
 
+using System.IO;
 using Microsoft.Data.Sqlite;
 
 public abstract class DatabaseOperatorBase : IDatabaseOperator
 {
+    private SqliteConnection _sqliteConnection;
 
+    public DatabaseOperatorBase(string dbPath, bool createFlag)
+    {
+        bool dbPathExists = File.Exists(dbPath);
+        string connectionString = new SqliteConnectionStringBuilder { DataSource = dbPath }.ToString();
+        if(!createFlag && !dbPathExists)
+        {
+            throw new FileNotFoundException($"'{dbPath}' does not exists.");
+        }
+        _sqliteConnection = new SqliteConnection(connectionString);
+        if(createFlag && !dbPathExists)
+        {
+            InitDatabase();
+        }
+    }
+    public DatabaseOperatorBase(string dbPath) : this(dbPath, false) {}
+
+    public Stream GetDataStream(byte[] index)
+    {
+        throw new NotImplementedException();
+    }
+    public void InsertData(byte[] index, byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected void InitDatabase()
+    {
+        throw new NotImplementedException();
+    }
+    private bool IsIndexExists(byte[] index)
+    {
+        throw new NotImplementedException();
+    }
 }
