6c604dc26a492b2fdaa197467f6ea20cca0c49e8
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-07 18:34:46 +0900

    test: Added test "CheckNonExistentIndex_ReturnFalse".
    All tests passed.

--- LibEncryptedDriveScripts.Tests/IDatabaseOperator_CommonTests.cs
+++ LibEncryptedDriveScripts.Tests/IDatabaseOperator_CommonTests.cs
@@ -100,6 +100,15 @@ public class IDatabaseOperator_CommonTests : IDisposable
         Assert.True(result);
     }
 
+    [Theory]
+    [MemberData(nameof(IDatabaseOperatorObjects))]
+    public void CheckNonExistentIndex_ReturnFalse(IDatabaseOperator dbOperator, string className)
+    {
+        byte[] nonExistentIndex = {255,0,0,7};
+        bool result = dbOperator.IsIndexExists(nonExistentIndex);
+        Assert.False(result);
+    }
+
     public void Dispose()
     {
         DeleteFileIfExists(dbPathDbOpeBaseForTestPath);
