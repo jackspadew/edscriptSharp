cb61f46cb400f12cef157080a61b55dc3a79b7b1
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-10 21:22:16 +0900

    add: Added class "RandomBlendConverter".

--- LibEncryptedDriveScripts/converter/BytesConverter.cs
+++ LibEncryptedDriveScripts/converter/BytesConverter.cs
@@ -7,3 +7,24 @@ public class JustReturnConverter<T> : IConverter<T, T>
         return input;
     }
 }
+
+public class RandomBlendConverter : IConverter<byte[], byte[]>
+{
+    protected Random _random;
+
+    public RandomBlendConverter(Random random)
+    {
+        _random = random;
+    }
+
+    public byte[] Convert(byte[] input)
+    {
+        byte[] buffer = new byte[input.Length];
+        _random.NextBytes(buffer);
+        for(int i=0; i < input.Length; i++)
+        {
+            buffer[i] = (byte)(buffer[i] ^ input[i]);
+        }
+        return buffer;
+    }
+}
