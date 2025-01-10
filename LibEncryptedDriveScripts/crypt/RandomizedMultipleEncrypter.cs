d0f46be2c94c54bb156e8b5d14fc0f821780ed9a
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-10 21:25:41 +0900

    add: Implemented methods "GenerateKeyList" and "GenerateIVList" of class RandomizedMultipleEncrypter.

--- LibEncryptedDriveScripts/crypt/RandomizedMultipleEncrypter.cs
+++ LibEncryptedDriveScripts/crypt/RandomizedMultipleEncrypter.cs
@@ -1,11 +1,13 @@
 namespace LibEncryptedDriveScripts.SymmetricEncrypter;
 
+using LibEncryptedDriveScripts.Converter;
+using LibEncryptedDriveScripts.KeyGenerator;
 using LibEncryptedDriveScripts.SymmetricAlgorithmAdapter;
 
 public class RandomizedMultipleEncrypter : SymmetricEncrypterBase, ISymmetricEncrypter
 {
-    protected Random _keyRandom;
-    protected Random _ivRandom;
+    protected int _keySeed;
+    protected int _ivSeed;
     private static List<ISymmetricAlgorithmAdapter> AlgorithmCandidateList {get;} = new()
     {
         new SymmetricAlgorithmAdapter.SystemCryptography.AES(),
@@ -13,7 +15,23 @@ public class RandomizedMultipleEncrypter : SymmetricEncrypterBase, ISymmetricEnc
     };
     public RandomizedMultipleEncrypter(int keySeed, int ivSeed)
     {
-        _keyRandom = new Random(keySeed);
-        _ivRandom = new Random(ivSeed);
+        _keySeed = keySeed;
+        _ivSeed = ivSeed;
+    }
+
+    protected override List<byte[]> GenerateKeyList(byte[] key, int count)
+    {
+        return GenerateRandomBlendedSequentialBytesList(key,count,_keySeed);
+    }
+    protected override List<byte[]> GenerateIVList(byte[] iv, int count)
+    {
+        return GenerateRandomBlendedSequentialBytesList(iv,count,_ivSeed);
+    }
+    private List<byte[]> GenerateRandomBlendedSequentialBytesList(byte[] key, int count, int seed)
+    {
+        Random random = new Random(_keySeed);
+        var converter = new RandomBlendConverter(random);
+        var listGenerator = new SequentialGenerator<byte[]>(converter, key);
+        return listGenerator.Generate(count);
     }
 }
