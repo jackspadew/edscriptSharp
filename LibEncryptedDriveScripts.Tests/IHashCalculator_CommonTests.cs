f1935a1b77d19c5aa055ce6131e97b9c2b07c46a
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-11 21:37:53 +0900

    test: [ALL TESTS PASSED] Added test class "IHashCalculator_Tests".
    Added test for simple implementation class of abstract class "HashCalculatorBase".
    Added test for class "DynamicHashCalculator" with SHA3 of BouncyCastle.

---
+++ LibEncryptedDriveScripts.Tests/IHashCalculator_CommonTests.cs
@@ -0,0 +1,59 @@
+namespace LibEncryptedDriveScripts.Tests;
+
+using Xunit;
+using LibEncryptedDriveScripts.HashCalculator;
+using LibEncryptedDriveScripts.HashAlgorithmAdapter;
+
+#pragma warning disable xUnit1026 // Unused arguments
+
+public class IHashCalculator_Tests
+{
+    public class HashCalculator_ForTest : HashCalculatorBase, IHashCalculator
+    {
+        public HashCalculator_ForTest(IHashAlgorithmAdapter hashAlgorithm) : base(hashAlgorithm)
+        {}
+    }
+    public static byte[] exampleBytes = {0,1,0,0};
+    public static byte[] anotherBytes = {0,0,0,0};
+    public static byte[] exampleSalt = {0,0};
+    public static IEnumerable<object[]> IHashCalculatorObjects()
+    {
+        yield return new object[] { new HashCalculator_ForTest(new HashAlgorithmAdapter.BouncyCastle.SHA3()), "ExampleHashCalculator_WithBcSha3" };
+        yield return new object[] { new DynamicHashCalculator(new HashAlgorithmAdapter.BouncyCastle.SHA3()), "DynamicHashCalculator_WithBcSha3" };
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHash_WillReturnThatHaveEnoughLength(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes);
+        Assert.NotInRange(hash.Length, 0, 31);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashAnotherBytes_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes);
+        byte[] hashAnother = hashCalculator.ComputeHash(anotherBytes);
+        Assert.NotEqual(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashWithStretching_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleBytes, 2);
+        Assert.NotEqual(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashWithSalt_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleBytes, exampleSalt);
+        Assert.NotEqual(hash, hashAnother);
+    }
+}
