af95297af617a19d1360f2613edcf5bb2fe62920
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-12 02:22:47 +0900

    test: Added some tests to "IHashCalculator_Tests".
    Added some test patterns related to using Stream for method ComputeHash.

--- LibEncryptedDriveScripts.Tests/IHashCalculator_CommonTests.cs
+++ LibEncryptedDriveScripts.Tests/IHashCalculator_CommonTests.cs
@@ -18,7 +18,9 @@ public class IHashCalculator_Tests
         }
     }
     public static byte[] exampleBytes = {0,1,0,0};
+    public static MemoryStream exampleStream => new(exampleBytes);
     public static byte[] anotherBytes = {0,0,0,0};
+    public static MemoryStream anotherStream => new(anotherBytes);
     public static byte[] exampleSalt = {0,0};
     public static IEnumerable<object[]> IHashCalculatorObjects()
     {
@@ -36,10 +38,27 @@ public class IHashCalculator_Tests
 
     [Theory]
     [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashByStream_WillReturnThatHaveEnoughLength(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleStream);
+        Assert.NotInRange(hash.Length, 0, 31);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
     public void ComputeHashAnotherBytes_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
     {
         byte[] hash = hashCalculator.ComputeHash(exampleBytes);
         byte[] hashAnother = hashCalculator.ComputeHash(anotherBytes);
+        Assert.NotEqual(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashAnotherStream_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleStream);
+        byte[] hashAnother = hashCalculator.ComputeHash(anotherStream);
         Assert.NotEqual(hash, hashAnother);
     }
 
@@ -54,10 +73,57 @@ public class IHashCalculator_Tests
 
     [Theory]
     [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashByStreamWithStretching_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleStream);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleStream, 2);
+        Assert.NotEqual(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
     public void ComputeHashWithSalt_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
     {
         byte[] hash = hashCalculator.ComputeHash(exampleBytes);
         byte[] hashAnother = hashCalculator.ComputeHash(exampleBytes, exampleSalt);
         Assert.NotEqual(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashByStreamWithSalt_WillReturnAnotherHash(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleStream);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleStream, exampleSalt);
+        Assert.NotEqual(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashByBytesAndStream_ReturnHashIsEqual(IHashCalculator hashCalculator, string className)
+    {
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleStream);
+        Assert.Equal(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashByBytesAndStreamWithStretch_ReturnHashIsEqual(IHashCalculator hashCalculator, string className)
+    {
+        int stretchCount = 10;
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes, stretchCount);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleStream, stretchCount);
+        Assert.Equal(hash, hashAnother);
+    }
+
+    [Theory]
+    [MemberData(nameof(IHashCalculatorObjects))]
+    public void ComputeHashByBytesAndStreamWithStretchAndSalt_ReturnHashIsEqual(IHashCalculator hashCalculator, string className)
+    {
+        int stretchCount = 10;
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes, exampleSalt, stretchCount);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleStream, exampleSalt, stretchCount);
+        Assert.Equal(hash, hashAnother);
     }
 }
