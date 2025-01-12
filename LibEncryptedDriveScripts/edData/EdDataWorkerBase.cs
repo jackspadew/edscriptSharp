22a49eaafefedf564fa3075e2b6b45a8cbfcbcf5
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-13 02:00:54 +0900

    add: Added abstract class "EdDataWorkerBase".

---
+++ LibEncryptedDriveScripts/edData/EdDataWorkerBase.cs
@@ -0,0 +1,28 @@
+namespace LibEncryptedDriveScripts.EdData;
+
+using LibEncryptedDriveScripts.Database;
+
+public abstract class EdDataWorkerBase : IEdDataExtractor, IEdDataPlanter
+{
+    private IDatabaseOperator _dbOperator;
+    private IEdDataCryptor _edCryptor;
+    public EdDataWorkerBase(IDatabaseOperator dbOperator, IEdDataCryptor edCryptor)
+    {
+        _dbOperator = dbOperator;
+        _edCryptor = edCryptor;
+    }
+    public virtual void Insert(string index, byte[] data, IMultipleKeyExchanger multiKey, byte[] key)
+    {
+        byte[] encryptedBytes = _edCryptor.EncryptBytes(data, multiKey);
+        byte[] indexBytes = GenerateIndexBytes(index);
+        _dbOperator.InsertData(indexBytes, encryptedBytes);
+    }
+    public virtual byte[] Extract(string index, IMultipleKeyExchanger multiKey, byte[] key)
+    {
+        byte[] indexBytes = GenerateIndexBytes(index);
+        byte[] stashedBytes = _dbOperator.GetDataBytes(indexBytes);
+        byte[] sourceBytes = _edCryptor.DecryptBytes(stashedBytes, multiKey);
+        return sourceBytes;
+    }
+    protected abstract byte[] GenerateIndexBytes(string name);
+}
