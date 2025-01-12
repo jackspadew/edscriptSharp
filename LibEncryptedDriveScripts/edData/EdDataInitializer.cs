880ac4e6123ca8c6b77a47bec389c9f305e8c59f
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-13 02:14:47 +0900

    add: Defined class "EdDataInitializer".
    Defined the bare minimum.

---
+++ LibEncryptedDriveScripts/edData/EdDataInitializer.cs
@@ -0,0 +1,13 @@
+namespace LibEncryptedDriveScripts.EdData;
+
+using LibEncryptedDriveScripts.Database;
+public class EdDataInitializer : EdDataWorkerBase, IEdDataExtractor, IEdDataPlanter
+{
+    public EdDataInitializer(IDatabaseOperator dbOperator, IEdDataCryptor edCryptor) : base(dbOperator, edCryptor)
+    {}
+
+    protected override byte[] GenerateIndexBytes(string name)
+    {
+        throw new NotImplementedException();
+    }
+}
