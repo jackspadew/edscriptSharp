4710cff0e78840676b56ce6df2363f5602638df9
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-14 00:15:41 +0900

    test: Added test class "RandomPickedListGenerator_Tests".
    Added test "GenerateUsingIntList_TheRatiosInReturnedListAreIdeal".

---
+++ LibEncryptedDriveScripts.Tests/RandomPickedListGeneratorTests.cs
@@ -0,0 +1,30 @@
+namespace LibEncryptedDriveScripts.Tests;
+
+using Xunit;
+using LibEncryptedDriveScripts.KeyGenerator;
+
+public class RandomPickedListGenerator_Tests
+{
+    private int SpecifiedLengthNumber = 1000;
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    public void GenerateUsingIntList_TheRatiosInReturnedListAreIdeal(int seed)
+    {
+        var exampleList = new List<int>(){0,1,2,3};
+        var generator = new RandomPickedListGenerator<int>(exampleList, seed);
+        var list = generator.Generate(SpecifiedLengthNumber);
+        var previousValue = list[0];
+        foreach(var value in exampleList)
+        {
+            int countOfValue = list.Count(x => x == value);
+            double resultRetio = (double)countOfValue / SpecifiedLengthNumber;
+            double theoreticalRatio = 1 / (double)exampleList.Count;
+            Assert.InRange(resultRetio, theoreticalRatio - 0.05, theoreticalRatio + 0.05);
+        }
+    }
+}
