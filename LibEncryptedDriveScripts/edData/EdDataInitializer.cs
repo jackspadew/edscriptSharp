eca7ea1281ce17e30939df359d1c12b8c8259020
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-16 04:03:37 +0900

    style: Minor corrections regarding coding style.

--- LibEncryptedDriveScripts/edData/EdDataInitializer.cs
+++ LibEncryptedDriveScripts/edData/EdDataInitializer.cs
@@ -1,6 +1,7 @@
 namespace LibEncryptedDriveScripts.EdData;
 
 using LibEncryptedDriveScripts.Database;
+
 public class EdDataInitializer : EdDataWorkerBase, IEdDataExtractor, IEdDataPlanter
 {
     public EdDataInitializer(IDatabaseOperator dbOperator, IEdDataCryptor edCryptor) : base(dbOperator, edCryptor)
