de8959522f3882ce0fc1cab5edc9c991da70a7b8
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-15 22:32:48 +0900

    feat: Implemented feature Lye Randomizer to "RandomizedHashCalculator".
    Added the feature to randomize the property "LyeBytes".

--- LibEncryptedDriveScripts/hash/RandomizedHashCalculator.cs
+++ LibEncryptedDriveScripts/hash/RandomizedHashCalculator.cs
@@ -2,6 +2,7 @@ namespace LibEncryptedDriveScripts.HashCalculator;
 
 using LibEncryptedDriveScripts.HashAlgorithmAdapter;
 using LibEncryptedDriveScripts.KeyGenerator;
+using LibEncryptedDriveScripts.Converter;
 
 public class RandomizedHashCalculator : LyeHashCalculatorBase, IHashCalculator
 {
@@ -17,11 +18,31 @@ public class RandomizedHashCalculator : LyeHashCalculatorBase, IHashCalculator
             return value;
         }
         }
+    private byte[] _lyeOriginBytes = new byte[0];
+    private IEnumerator<byte[]> _lyeBytesEnumrator;
+    private List<byte[]> _lyeBytesList = new();
+    protected override byte[] LyeBytes {
+        get
+        {
+            if(_lyeBytesList.Count == 0) return _lyeOriginBytes;
+            var value = _lyeBytesEnumrator.Current;
+            _lyeBytesEnumrator.MoveNext();
+            return value;
+        }
+        set
+        {
+            _lyeOriginBytes = value;
+        }
+        }
     protected int _seed=0;
-    public RandomizedHashCalculator(int seed) : base()
+    public RandomizedHashCalculator(int seed) : this(new byte[0], seed)
+    {}
+    public RandomizedHashCalculator(byte[] lyeBytes, int seed)
     {
         _seed = seed;
         _algorithmEnumrator = _algorithmSequentialList.GetEnumerator();
+        _lyeBytesEnumrator = _lyeBytesList.GetEnumerator();
+        _lyeOriginBytes = lyeBytes;
     }
     public override byte[] ComputeHash(byte[] inputBytes, int stretchCount = 1)
     {
@@ -39,5 +60,20 @@ public class RandomizedHashCalculator : LyeHashCalculatorBase, IHashCalculator
         _algorithmSequentialList = generator.Generate(length);
         _algorithmEnumrator = _algorithmSequentialList.GetEnumerator();
         _algorithmEnumrator.MoveNext();
+    }
+    private void GenerateLyeSequentialList(int length)
+    {
+        Random random = new Random(_seed);
+        byte[] initialLye = _lyeOriginBytes;
+        var converter = new RandomBlendConverter(random);
+        var generator = new SequentialGenerator<byte[]>(converter, initialLye);
+        _lyeBytesList = generator.Generate(length);
+        _lyeBytesEnumrator = _lyeBytesList.GetEnumerator();
+        _lyeBytesEnumrator.MoveNext();
+    }
+    protected override byte[] Stretching(byte[] hashBytes, int stretchCount)
+    {
+        GenerateLyeSequentialList(stretchCount-1);
+        return base.Stretching(hashBytes, stretchCount);
     }
 }
