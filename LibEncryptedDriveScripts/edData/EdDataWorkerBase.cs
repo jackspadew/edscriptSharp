dc8c3f20cf73e47cf69d2fa5858e216085213305
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-16 03:41:40 +0900

    refactor: Changed abstract class "EdDataWorkerBase".

--- LibEncryptedDriveScripts/edData/EdDataWorkerBase.cs
+++ LibEncryptedDriveScripts/edData/EdDataWorkerBase.cs
@@ -4,25 +4,21 @@ using LibEncryptedDriveScripts.Database;
 
 public abstract class EdDataWorkerBase : IEdDataExtractor, IEdDataPlanter
 {
-    private IDatabaseOperator _dbOperator;
-    private IEdDataCryptor _edCryptor;
-    public IMultipleKeyExchanger _multipleKey;
-    public EdDataWorkerBase(IDatabaseOperator dbOperator, IEdDataCryptor edCryptor)
-    {
-        _dbOperator = dbOperator;
-        _edCryptor = edCryptor;
-    }
+    protected abstract IDatabaseOperator DbOperator {get;}
+    protected abstract IEdDataCryptor EdCryptor {get;}
+    protected abstract IMultipleKeyExchanger MultipleKey {get;}
+
     public virtual void Stash(string index, byte[] data)
     {
-        byte[] encryptedBytes = _edCryptor.EncryptBytes(data, _multipleKey);
+        byte[] encryptedBytes = EdCryptor.EncryptBytes(data, MultipleKey);
         byte[] indexBytes = GenerateIndexBytes(index);
-        _dbOperator.InsertData(indexBytes, encryptedBytes);
+        DbOperator.InsertData(indexBytes, encryptedBytes);
     }
     public virtual byte[] Extract(string index)
     {
         byte[] indexBytes = GenerateIndexBytes(index);
-        byte[] stashedBytes = _dbOperator.GetDataBytes(indexBytes);
-        byte[] sourceBytes = _edCryptor.DecryptBytes(stashedBytes, _multipleKey);
+        byte[] stashedBytes = DbOperator.GetDataBytes(indexBytes);
+        byte[] sourceBytes = EdCryptor.DecryptBytes(stashedBytes, MultipleKey);
         return sourceBytes;
     }
     protected abstract byte[] GenerateIndexBytes(string name);
