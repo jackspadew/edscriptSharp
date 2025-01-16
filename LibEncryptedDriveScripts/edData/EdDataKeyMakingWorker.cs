c10cd3e0802b537f63be8f6eecaf8f07e85babf6
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-17 02:45:00 +0900

    add: Implemented class "EdDataKeyMakingWorker" roughly.
    Defined a constructor.

--- LibEncryptedDriveScripts/edData/EdDataKeyMakingWorker.cs
+++ LibEncryptedDriveScripts/edData/EdDataKeyMakingWorker.cs
@@ -1,15 +1,26 @@
 namespace LibEncryptedDriveScripts.EdData;
 
 using LibEncryptedDriveScripts.Database;
+using LibEncryptedDriveScripts.HashCalculator;
 
 public class EdDataKeyMakingWorker : EdDataWorkerBase, IEdDataWorker
 {
-    protected override IDatabaseOperator DbOperator => throw new NotImplementedException();
-
-    protected override IEdDataCryptor EdCryptor => throw new NotImplementedException();
-
-    protected override IMultipleKeyExchanger MultipleKey => throw new NotImplementedException();
-
+    private readonly int MultipleEncryptionCount = 1000;
+    private readonly int HashStretchingCount = 1000;
+    private IDatabaseOperator _dbOperator;
+    protected override IDatabaseOperator DbOperator { get => _dbOperator; }
+    private IEdDataCryptor _edCryptor;
+    protected override IEdDataCryptor EdCryptor { get => _edCryptor; }
+    private IMultipleKeyExchanger _multipleKey;
+    protected override IMultipleKeyExchanger MultipleKey { get => _multipleKey; }
+    private IHashCalculator _hashCalculator;
+    public EdDataKeyMakingWorker(IDatabaseOperator dbOperator)
+    {
+        _dbOperator = dbOperator;
+        _multipleKey = new KeyMakerMultipleKeyExchanger();
+        _edCryptor = new EdDataCryptor(MultipleEncryptionCount);
+        _hashCalculator = new RandomizedHashCalculator(new byte[64], MultipleKey.HashSeed);
+    }
     protected override byte[] GenerateIndexBytes(string name)
     {
         throw new NotImplementedException();
