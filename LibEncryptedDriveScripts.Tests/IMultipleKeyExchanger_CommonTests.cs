55424b0ba51592e0dccfab710ad9848603e04909
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-17 00:26:15 +0900

    test: Added test "CopyToLikeRoundTrip_AllValueIsEqual" on "IMultipleKeyExchanger_CommonTests".

--- LibEncryptedDriveScripts.Tests/IMultipleKeyExchanger_CommonTests.cs
+++ LibEncryptedDriveScripts.Tests/IMultipleKeyExchanger_CommonTests.cs
@@ -25,4 +25,25 @@ public class IMultipleKeyExchanger_CommonTests
         Assert.Equal(sourceBytes.Length, exchangedBytes.Length);
         Assert.Equal(sourceBytes, exchangedBytes);
     }
+
+    [Theory]
+    [MemberData(nameof(IMultipleKeyExchangerObjects))]
+    public void CopyToLikeRoundTrip_AllValueIsEqual(IMultipleKeyExchanger multiKey, string className)
+    {
+        Random random = new Random(0);
+        multiKey.KeySeed = random.Next();
+        multiKey.IVSeed = random.Next();
+        multiKey.AlgorithmSeed = random.Next();
+        multiKey.HashSeed = random.Next();
+        random.NextBytes(multiKey.Key);
+        random.NextBytes(multiKey.IV);
+        var targetMultiKey = new MultipleKeyExchangerBase_Concrete();
+        multiKey.CopyTo(targetMultiKey);
+        Assert.Equal(multiKey.KeySeed, targetMultiKey.KeySeed);
+        Assert.Equal(multiKey.IVSeed, targetMultiKey.IVSeed);
+        Assert.Equal(multiKey.AlgorithmSeed, targetMultiKey.AlgorithmSeed);
+        Assert.Equal(multiKey.HashSeed, targetMultiKey.HashSeed);
+        Assert.Equal(multiKey.Key, targetMultiKey.Key);
+        Assert.Equal(multiKey.IV, targetMultiKey.IV);
+    }
 }
