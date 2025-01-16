98bee2c30a49fe16e5ababaed8aa59825dde23b8
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-16 23:48:54 +0900

    add: Implemented method "SetMultipleKey" on "EdDataWorkerBase".

--- LibEncryptedDriveScripts/edData/EdDataWorkerBase.cs
+++ LibEncryptedDriveScripts/edData/EdDataWorkerBase.cs
@@ -27,4 +27,8 @@ public abstract class EdDataWorkerBase : IEdDataWorker
         return DbOperator.IsIndexExists(indexBytes);
     }
     protected abstract byte[] GenerateIndexBytes(string name);
+    public virtual void SetMultipleKey(IMultipleKeyExchanger multiKey)
+    {
+        multiKey.CopyTo(MultipleKey);
+    }
 }
