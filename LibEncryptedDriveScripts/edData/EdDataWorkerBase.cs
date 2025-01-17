7bc82de33c78943350ab4f652065b5c816a4dc1c
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-18 03:21:15 +0900

    refactor: Changed abstract class "EdDataWorkerBase" to depend on interface "IEdDataLogicFactory".
    Object creation related to logic is done through interface "IEdDataLogicFactory".

--- LibEncryptedDriveScripts/edData/EdDataWorkerBase.cs
+++ LibEncryptedDriveScripts/edData/EdDataWorkerBase.cs
@@ -1,12 +1,43 @@
 namespace LibEncryptedDriveScripts.EdData;
 
 using LibEncryptedDriveScripts.Database;
+using System.Text;
 
 public abstract class EdDataWorkerBase : IEdDataWorker
 {
-    protected abstract IDatabaseOperator DbOperator {get;}
-    protected abstract IEdDataCryptor EdCryptor {get;}
-    protected abstract IMultipleKeyExchanger MultipleKey {get;}
+    private IDatabaseOperator _dbOperator;
+    protected virtual IDatabaseOperator DbOperator {
+        get {
+            return _dbOperator;
+        }
+        }
+    private IEdDataCryptor _edCryptor;
+    protected virtual IEdDataCryptor EdCryptor {
+        get {
+            return _edCryptor;
+        }
+        }
+    private IMultipleKeyExchanger _multipleKey;
+    protected virtual IMultipleKeyExchanger MultipleKey {
+        get {
+            return _multipleKey;
+        }
+        }
+    private IEdDataHashCalculator _hashCalculator;
+    protected virtual IEdDataHashCalculator HashCalculator {
+        get {
+            return _hashCalculator;
+        }
+        }
+    protected IEdDataLogicFactory _logicFactory;
+    public EdDataWorkerBase(IEdDataLogicFactory logicFactory)
+    {
+        _logicFactory = logicFactory;
+        _dbOperator = logicFactory.CreateDatabaseOperator();
+        _edCryptor = logicFactory.CreateCryptor();
+        _multipleKey = logicFactory.CreateMultipleKeyExchanger();
+        _hashCalculator = logicFactory.CreateHashCalculator();
+    }
 
     public virtual void Stash(string index, byte[] data)
     {
@@ -26,5 +57,10 @@ public abstract class EdDataWorkerBase : IEdDataWorker
         byte[] indexBytes = GenerateIndexBytes(index);
         return DbOperator.IsIndexExists(indexBytes);
     }
-    protected abstract byte[] GenerateIndexBytes(string name);
+    protected virtual byte[] GenerateIndexBytes(string name)
+    {
+        byte[] nameBytes = Encoding.UTF8.GetBytes(name);
+        byte[] rawIndexBytes = nameBytes;
+        return _hashCalculator.ComputeHash(rawIndexBytes);
+    }
 }
