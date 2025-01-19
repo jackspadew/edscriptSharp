d3bed1b9142f0a7a970ba49113c5a34fb8a160d9
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-19 14:51:55 +0900

    refactor: Changed "EdDataWorkerChainBase" to inherit from "EdDataWorkerBase".

--- LibEncryptedDriveScripts/edData/EdDataWorkerChainBase.cs
+++ LibEncryptedDriveScripts/edData/EdDataWorkerChainBase.cs
@@ -1,15 +1,13 @@
 namespace LibEncryptedDriveScripts.EdData;
 
-public abstract class EdDataWorkerChainBase : IEdDataWorker, IEdDataWorkerChain
+public abstract class EdDataWorkerChainBase : EdDataWorkerBase, IEdDataWorker, IEdDataWorkerChain
 {
     private int _depth;
     public int Depth { get => _depth; }
     protected IEdDataWorker _parentWorker;
-    protected IEdDataLogicFactory _logicFactory;
 
-    protected EdDataWorkerChainBase(IEdDataLogicFactory logicFactory, IEdDataWorker parentWorker)
+    protected EdDataWorkerChainBase(IEdDataLogicFactory logicFactory, IEdDataWorker parentWorker) : base(logicFactory)
     {
-        _logicFactory = logicFactory;
         _parentWorker = parentWorker;
         if(_parentWorker is IEdDataWorkerChain worker)
         {
