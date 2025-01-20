02196ecce2266e66ecc96b79f595fa2d7cc1265e
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-21 03:20:25 +0900

    style: Changed the definition of class "InitialMultipleKeyExchanger" to be done in another file.

--- LibEncryptedDriveScripts/edData/EdDataInitialWorker.cs
+++ LibEncryptedDriveScripts/edData/EdDataInitialWorker.cs
@@ -27,20 +27,3 @@ public class EdDataInitialWorker : EdDataWorkerBase, IEdDataWorker, IEdDataWorke
         return initMultiKey;
     }
 }
-
-public class InitialMultipleKeyExchanger : MultipleKeyExchangerBase, IMultipleKeyExchanger
-{
-    public InitialMultipleKeyExchanger()
-    {
-        Random random = new Random(20626197);
-        KeySeed = 90849388;
-        IVSeed = 88871264;
-        AlgorithmSeed = 93476436;
-        byte[] key = new byte[32];
-        random.NextBytes(key);
-        byte[] iv = new byte[16];
-        random.NextBytes(iv);
-        Key = key;
-        IV = iv;
-    }
-}
---
+++ LibEncryptedDriveScripts/edData/implementation_MultipleKeyExchanger.cs
@@ -0,0 +1,18 @@
+namespace LibEncryptedDriveScripts.EdData;
+
+public class InitialMultipleKeyExchanger : MultipleKeyExchangerBase, IMultipleKeyExchanger
+{
+    public InitialMultipleKeyExchanger()
+    {
+        Random random = new Random(20626197);
+        KeySeed = 90849388;
+        IVSeed = 88871264;
+        AlgorithmSeed = 93476436;
+        byte[] key = new byte[32];
+        random.NextBytes(key);
+        byte[] iv = new byte[16];
+        random.NextBytes(iv);
+        Key = key;
+        IV = iv;
+    }
+}
