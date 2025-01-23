f73cf66d8ab7f0937851c4e444f053b5e567d8f7
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-23 10:32:07 +0900

    test: [PASSED] Added test method "StashMultiplePiecesData_NotThrow" to IntegrationTests_EdData.
    Defined private methods "GenerateRandomString" and "GenerateRandomStringList".

--- LibEncryptedDriveScripts.Tests/IntegrationTests_EdData.cs
+++ LibEncryptedDriveScripts.Tests/IntegrationTests_EdData.cs
@@ -1,6 +1,7 @@
 namespace LibEncryptedDriveScripts.Tests;
 
 using LibEncryptedDriveScripts.EdData;
+using System.Text;
 
 public class IntegrationTests_EdData
 {
@@ -17,6 +18,30 @@ public class IntegrationTests_EdData
         return worker;
     }
 
+    private string GenerateRandomString(int length, int seed)
+    {
+        const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder(length);
+        Random random = new Random(seed);
+        for (int i = 0; i < length; i++)
+        {
+            int pos = random.Next(chars.Length);
+            sb.Append(chars[pos]);
+        }
+        return sb.ToString();
+    }
+    private List<string> GenerateRandomStringList(int stringLength, int listCount)
+    {
+        Random random = new Random(0);
+        var list = new List<string>();
+        for(int i=0; i<listCount; i++)
+        {
+            string randomStr = GenerateRandomString(stringLength, random.Next());
+            list.Add(randomStr);
+        }
+        return list;
+    }
+
     [Fact]
     public void StashData_ItCanBeExtracted()
     {
@@ -26,5 +51,18 @@ public class IntegrationTests_EdData
         var workerForExtract = DoCreateWorkerForTest();
         byte[] extractedBytes = workerForExtract.Extract(exampleIndex);
         Assert.Equal(exampleBytes, extractedBytes);
+    }
+
+    [Fact]
+    public void StashMultiplePiecesData_NotThrow()
+    {
+        int stashingCount = 10;
+        CommonFunctions.DeleteFileIfExists(dbPath);
+        var randomIndexNameList = GenerateRandomStringList(256, stashingCount);
+        var workerForStash = DoCreateWorkerForTest();
+        foreach(string str in randomIndexNameList)
+        {
+            workerForStash.Stash(str, exampleBytes);
+        }
     }
 }
