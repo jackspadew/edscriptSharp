86206b6fad016843fb53709ba33f2e56065945b4
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-23 20:54:08 +0900

    refactor: [ALL TESTS PASSED] Rewrote class "AES" that using Bouncy Castle engine.
    The existing definition of class "AES" was deleted.
    A new class "AES" is defined that inherits abstract class "BouncyCastleEngineAdapterBase".

--- LibEncryptedDriveScripts/SymmetricAlgorithmAdapter/implementation_BouncyCastleAlgorithmAdapter.cs
+++ LibEncryptedDriveScripts/SymmetricAlgorithmAdapter/implementation_BouncyCastleAlgorithmAdapter.cs
@@ -3,6 +3,11 @@ namespace LibEncryptedDriveScripts.SymmetricAlgorithmAdapter.BouncyCastle;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Engines;
 
+public class AES : BouncyCastleEngineAdapterBase, ISymmetricAlgorithmAdapter
+{
+    protected override IBlockCipher BCCryptoEngine => new AesEngine();
+}
+
 public class Camellia : BouncyCastleEngineAdapterBase, ISymmetricAlgorithmAdapter
 {
     protected override IBlockCipher BCCryptoEngine => new CamelliaEngine();
