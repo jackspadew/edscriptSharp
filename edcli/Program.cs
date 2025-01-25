3f8b532fbd80180edb1a1cefe13694ba0a91c6d5
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-26 00:10:24 +0900

    add: Created class "Program" and defined entry point "Main".

--- edcli/Program.cs
+++ edcli/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace edcli;
+
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
