d8efef614d39f85119eda66dc5c583d4648be63e
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-26 21:25:08 +0900

    add: Implemented method "Stash","Extract" of class "Program".
    If the input value is correct, it works well.
    Error handling is not implemented.

--- edcli/Program_EdData.cs
+++ edcli/Program_EdData.cs
@@ -1,6 +1,7 @@
 namespace edcli;
 
 using LibEncryptedDriveScripts.EdData;
+using System.Text;
 
 public partial class Program
 {
@@ -14,10 +15,15 @@ public partial class Program
     {
         string password = nullablePassword ?? ReadPassword();
         Console.WriteLine($"Do stash with name=\"{indexName}\", file=\"{stashTargetFileInfo.FullName}\" password=\"{password}\".");
+        byte[] fileBytes = Encoding.UTF8.GetBytes(File.ReadAllText(stashTargetFileInfo.FullName));
+        CreateEdDataWorker(dbFileInfo.FullName, password).Stash(indexName, fileBytes);
     }
     public static void Extract(FileInfo dbFileInfo, string indexName, string? nullablePassword)
     {
         string password = nullablePassword ?? ReadPassword();
         Console.WriteLine($"Do extract with name=\"{indexName}\", password=\"{password}\".");
+        byte[] data = CreateEdDataWorker(dbFileInfo.FullName, password).Extract(indexName);
+        string rawString = Encoding.UTF8.GetString(data);
+        Console.WriteLine(rawString);
     }
 }
