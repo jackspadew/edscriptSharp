f0e45c254b2e1122e5e57e203b6708c346c7fe00
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-28 18:45:46 +0900

    test: Added test "ComputeHashWithAnotherSeedMultipleKey_ReturnAnotherBytes".

--- LibEncryptedDriveScripts.Tests/EdData/IEdDataHashCalculator_CommonTests.cs
+++ LibEncryptedDriveScripts.Tests/EdData/IEdDataHashCalculator_CommonTests.cs
@@ -97,4 +97,17 @@ public class IEdDataHashCalculator_CommonTests
         byte[] hashAnother = hashCalculator.ComputeHash(exampleBytes, anotherMultiKey);
         Assert.NotEqual(hash, hashAnother);
     }
+
+    [Theory]
+    [MemberData(nameof(IEdDataHashCalculator_Objects))]
+    public void ComputeHashWithAnotherSeedMultipleKey_ReturnAnotherBytes(IEdDataHashCalculator hashCalculator, string className)
+    {
+        IMultipleKeyExchanger multiKey = GetRandomizedMultipleKey();
+        IMultipleKeyExchanger anotherMultiKey = new BasicExemplaryMultipleKeyExchanger();
+        multiKey.CopyTo(anotherMultiKey);
+        anotherMultiKey.HashSeed += 1;
+        byte[] hash = hashCalculator.ComputeHash(exampleBytes, multiKey);
+        byte[] hashAnother = hashCalculator.ComputeHash(exampleBytes, anotherMultiKey);
+        Assert.NotEqual(hash, hashAnother);
+    }
 }
