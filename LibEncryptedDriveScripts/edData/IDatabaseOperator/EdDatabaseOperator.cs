66885825895bd16556f6cd942afdb140fe71cd5a
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-30 18:09:57 +0900

    add: Added  override methods "InsertData" to "EdDatabaseOperator".

--- LibEncryptedDriveScripts/edData/IDatabaseOperator/EdDatabaseOperator.cs
+++ LibEncryptedDriveScripts/edData/IDatabaseOperator/EdDatabaseOperator.cs
@@ -1,9 +1,19 @@
 namespace LibEncryptedDriveScripts.EdData;
 
+using System.IO;
 using LibEncryptedDriveScripts.Database;
 
 public class EdDatabaseOperator : DatabaseOperatorBase, IDatabaseOperator
 {
     public EdDatabaseOperator(string dbPath, bool createFlag) : base(dbPath, createFlag)
     {}
+
+    public override void InsertData(byte[] index, byte[] data)
+    {
+        base.InsertData(index, data);
+    }
+    public override void InsertData(byte[] index, Stream readableStream)
+    {
+        base.InsertData(index, readableStream);
+    }
 }
