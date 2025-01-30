d357ee093b3cb00efb33f4d8a3c2ea484c7660a2
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-30 23:11:26 +0900

    test: [PASSED] Added tests to "RandomizedExecutor_Tests".
    Added test "RunWithLongCountList_CalledCorrectTimes".
    Added test "RunWithShortCountList_CalledCorrectTimes".

--- LibEncryptedDriveScripts.Tests/Executor/RandomizedExecutorTests.cs
+++ LibEncryptedDriveScripts.Tests/Executor/RandomizedExecutorTests.cs
@@ -16,4 +16,28 @@ public class RandomizedExecutor_Tests
         mockedActOne.Verify(a => a(), Times.Exactly(countArray[0]));
         mockedActTwo.Verify(a => a(), Times.Exactly(countArray[1]));
     }
+
+    [Fact]
+    public void RunWithLongCountList_CalledCorrectTimes()
+    {
+        int[] countArray = new int[]{0,2,9,9,9};
+        var mockedActOne = new Mock<Action>();
+        var mockedActTwo = new Mock<Action>();
+        var executor = new RandomizedExecutor(countArray);
+        executor.Run([mockedActOne.Object,mockedActTwo.Object]);
+        mockedActOne.Verify(a => a(), Times.Exactly(countArray[0]));
+        mockedActTwo.Verify(a => a(), Times.Exactly(countArray[1]));
+    }
+
+    [Fact]
+    public void RunWithShortCountList_CalledCorrectTimes()
+    {
+        int[] countArray = new int[]{2};
+        var mockedActOne = new Mock<Action>();
+        var mockedActTwo = new Mock<Action>();
+        var executor = new RandomizedExecutor(countArray);
+        executor.Run([mockedActOne.Object,mockedActTwo.Object]);
+        mockedActOne.Verify(a => a(), Times.Exactly(countArray[0]));
+        mockedActTwo.Verify(a => a(), Times.Exactly(0));
+    }
 }
