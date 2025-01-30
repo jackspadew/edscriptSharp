6392360a4e37caa67b01624d77f6a2f545a59ce3
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-30 23:39:07 +0900

    add: Added class members for using "RandomizedExecutor".

--- LibEncryptedDriveScripts/edData/IDatabaseOperator/EdDatabaseOperator.cs
+++ LibEncryptedDriveScripts/edData/IDatabaseOperator/EdDatabaseOperator.cs
@@ -2,11 +2,18 @@ namespace LibEncryptedDriveScripts.EdData;
 
 using System.IO;
 using LibEncryptedDriveScripts.Database;
+using LibEncryptedDriveScripts.Executor;
 
 public class EdDatabaseOperator : DatabaseOperatorBase, IDatabaseOperator
 {
+    protected int InsertingFakeRowCount = 99;
+    protected IActionExecutor RandomExecutor => new RandomizedExecutor([1,InsertingFakeRowCount]);
     public EdDatabaseOperator(string dbPath, bool createFlag) : base(dbPath, createFlag)
     {}
+    public EdDatabaseOperator(string dbPath, bool createFlag, int insertingFakeCount) : base(dbPath, createFlag)
+    {
+        InsertingFakeRowCount = insertingFakeCount;
+    }
 
     public override void InsertData(byte[] index, byte[] data)
     {
