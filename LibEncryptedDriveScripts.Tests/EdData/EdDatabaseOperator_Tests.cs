fc59e01348de68da58376080295daeb3f889513f
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-31 06:32:51 +0900

    test: [PASSED] Added test class "EdDatabaseOperator_Tests".
    Added test "InsertData_CallingInsertFakeDataCountIsCorrect".

---
+++ LibEncryptedDriveScripts.Tests/EdData/EdDatabaseOperator_Tests.cs
@@ -0,0 +1,24 @@
+namespace LibEncryptedDriveScripts.Tests;
+
+using LibEncryptedDriveScripts.EdData;
+using Xunit;
+using Moq;
+using Moq.Protected;
+
+public class EdDatabaseOperator_Tests
+{
+    private static string dbPath = "EdDatabaseOperator_Tests.db";
+    private static byte[] exampleIndexBytes = new byte[]{255,0,0,1};
+    private static byte[] exampleData = new byte[]{0,1,2,3,4};
+
+    [Fact]
+    public void InsertData_CallingInsertFakeDataCountIsCorrect()
+    {
+        CommonFunctions.DeleteFileIfExists(dbPath);
+        int executionCount = 9;
+        var mockedDbOperator = new Mock<EdDatabaseOperator>(dbPath, true, executionCount){ CallBase = true };
+        mockedDbOperator.Object.InsertData(exampleIndexBytes, exampleData);
+        int actualCount = executionCount;
+        mockedDbOperator.Protected().Verify("InsertFakeData", Times.Exactly(actualCount), [exampleIndexBytes, exampleData]);
+    }
+}
