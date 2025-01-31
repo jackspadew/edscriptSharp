655519d676dfe0f1d8da18ed2017955812197640
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-01-31 19:44:38 +0900

    test: Removed test "StashThenExtractWithLongChain_CompleteWithinTime".

--- LibEncryptedDriveScripts.Tests/EdData/EdDataWorkerChainBase_Tests.cs
+++ LibEncryptedDriveScripts.Tests/EdData/EdDataWorkerChainBase_Tests.cs
@@ -83,28 +83,6 @@ public class EdDataWorkerChainBase_Tests
     }
 
     [Fact]
-    public void StashThenExtractWithLongChain_CompleteWithinTime()
-    {
-        CommonFunctions.DeleteFileIfExists(dbPath);
-        int targetChainDepth = 3;
-        var logic = new BasicEdDataLogicFactory(dbPath, examplePassword);
-        IEdDataWorker initialWorker = new EdDataInitialWorker(logic);
-        var workerChainZero = new EdDataWorkerChain(logic, initialWorker);
-        IEdDataWorkerChain nextWorker = workerChainZero;
-        for(int i=0; i<targetChainDepth; i++)
-        {
-            nextWorker = new EdDataWorkerChain(logic, nextWorker);
-        }
-        var workerLast = nextWorker;
-        byte[] extracted = new byte[0];
-        CommonFunctions.CompletesIn(5000, () => {
-            workerLast.Stash(exampleIndex, exampleByte);
-            extracted = workerLast.Extract(exampleIndex);
-        });
-        Assert.Equal(exampleByte, extracted);
-    }
-
-    [Fact]
     public void StashWithLongChain_CompleteWithinTime()
     {
         CommonFunctions.DeleteFileIfExists(dbPath);
