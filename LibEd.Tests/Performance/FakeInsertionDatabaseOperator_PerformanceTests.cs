c882f35c22a0f06037e81305a01f30b68abaf9c0
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-01 22:34:53 +0900

    test: Added performance test class "FakeInsertionDatabaseOperator_PerformanceTests".
    Added test "InsertData_CompleteWithinTime". The execution content or target time of this test is provisional.

---
+++ LibEd.Tests/Performance/FakeInsertionDatabaseOperator_PerformanceTests.cs
@@ -0,0 +1,23 @@
+namespace LibEd.PerformanceTests;
+
+using LibEd.EdData;
+using System.Reflection;
+using LibEd.Tests;
+
+#pragma warning disable CS8602
+
+public class FakeInsertionDatabaseOperator_PerformanceTests
+{
+    private static byte[] exampleByte = new byte[256];
+    private static byte[] exampleIndex = new byte[512];
+    [Fact]
+    public void InsertData_CompleteWithinTime()
+    {
+        string dbPath = MethodBase.GetCurrentMethod().Name + ".db";
+        CommonFunctions.DeleteFileIfExists(dbPath);
+        var dbOperator = new FakeInsertionDatabaseOperator(dbPath, true, 1000);
+        PerformanceTestCommon.CompletesIn( MethodBase.GetCurrentMethod().Name, 5000, () => {
+            dbOperator.InsertData(exampleIndex, exampleByte);
+        });
+    }
+}
