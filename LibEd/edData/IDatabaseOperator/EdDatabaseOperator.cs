588dcfe6e0c53bee85870d62d3551c15d03ada27
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-02 01:02:29 +0900

    refactor: Changed method "InsertData"(byte[]) to use FakeInsertion methods.

--- LibEd/edData/IDatabaseOperator/EdDatabaseOperator.cs
+++ LibEd/edData/IDatabaseOperator/EdDatabaseOperator.cs
@@ -28,7 +28,6 @@ public class FakeInsertionDatabaseOperator : DatabaseOperatorBase, IDatabaseOper
     {
         _sqliteConnection.Open();
         // Fake insertion variables
-        string fakeInsertionText = $"INSERT OR IGNORE INTO {_tableName} ({_indexName}, {_dataName}) VALUES (@{_indexName}, @{_dataName});";
         byte[] randomIndexBuffer = new byte[index.Length];
         byte[] randomDataBuffer = new byte[data.Length];
         RandomNumberGenerator rng = RandomNumberGenerator.Create();
@@ -36,25 +35,14 @@ public class FakeInsertionDatabaseOperator : DatabaseOperatorBase, IDatabaseOper
         try{
             using (var transaction = _sqliteConnection.BeginTransaction())
             {
-                // Create fake insertion command
-                var fakeInsertionCommand = new SqliteCommand(fakeInsertionText ,_sqliteConnection, transaction);
-                var paramIndex = fakeInsertionCommand.CreateParameter();
-                paramIndex.ParameterName = $"@{_indexName}";
-                var paramData = fakeInsertionCommand.CreateParameter();
-                paramData.ParameterName = $"@{_dataName}";
-                fakeInsertionCommand.Parameters.Add(paramIndex);
-                fakeInsertionCommand.Parameters.Add(paramData);
+                var fakeInsertionVariables = CreateFakeInsertionVariables(_sqliteConnection, transaction);
                 // Execute commands
                 RandomExecutor.Run([
                     () => {
                         ExecuteRealInsertionCommand(_sqliteConnection, transaction, index, data);
                     },
                     () => {
-                        rng.GetBytes(randomIndexBuffer);
-                        paramIndex.Value = randomIndexBuffer;
-                        rng.GetBytes(randomDataBuffer);
-                        paramData.Value = randomDataBuffer;
-                        fakeInsertionCommand.ExecuteNonQuery();
+                        ExecuteFakeInsertionCommand(fakeInsertionVariables.command, fakeInsertionVariables.paramIndex, fakeInsertionVariables.paramData, randomIndexBuffer, randomDataBuffer, rng);
                     }
                 ]);
                 transaction.Commit();
