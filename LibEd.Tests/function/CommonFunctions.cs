c574c5c512627ef836f0f169bd5c427088f2ac15
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-01 21:43:33 +0900

    test: Removed method "CompletesIn" from class "CommonFunctions".

--- LibEd.Tests/function/CommonFunctions.cs
+++ LibEd.Tests/function/CommonFunctions.cs
@@ -1,7 +1,5 @@
 namespace LibEd.Tests;
 
-using System.Diagnostics;
-
 public static class CommonFunctions
 {
     public static void DeleteFileIfExists(string path)
@@ -10,32 +8,5 @@ public static class CommonFunctions
         {
             File.Delete(path);
         }
-    }
-
-    public static void CompletesIn(string testName, int targettime, Action action, int timeout=60000)
-    {
-        var stopwatch = Stopwatch.StartNew();
-        var task = Task.Run(action);
-        var completedInTime = Task.WaitAll(new[] { task }, TimeSpan.FromMilliseconds(timeout));
-        stopwatch.Stop();
-        if (task.Exception != null)
-        {
-            if (task.Exception.InnerExceptions.Count == 1)
-            {
-                throw task.Exception.InnerExceptions[0];
-            }
-            throw task.Exception;
-        }
-        if (!completedInTime)
-        {
-            Assert.Fail($"The task was not completed due to timeout(={timeout}ms). ");
-        }
-        string seconds = (stopwatch.ElapsedMilliseconds / 1000.0).ToString("F3");
-        string completionTimeMessage = $"completion time: {seconds} sec";
-        if (stopwatch.ElapsedMilliseconds > targettime)
-        {
-            Assert.Fail($"Task completion time exceeds the target time(target time={targettime}ms). ({completionTimeMessage})");
-        }
-        Console.WriteLine($"[{testName}] {completionTimeMessage}");
     }
 }
