0cc4fc9dec1b5ec9311fae81e87c8e43d633e0b0
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-05 01:38:08 +0900

    test: Updated test "PsEdScript_CmdletTests".

--- PsEdScript/test/CmdletTests.ps1
+++ PsEdScript/test/CmdletTests.ps1
@@ -5,14 +5,26 @@ Import-Module $modulePath
 
 Describe 'PsEdScript_CmdletTests' {
     BeforeAll {
+        $dbPath = "example.db";
+        Remove-Item $dbPath
         Mock -CommandName "Read-Host" -MockWith { return "Mocked Input" }
     }
+
+    Context 'SetPsEdScript' {
+        It 'Will not throw' {
+            { "world" | Set-PsEdScript -IndexName "hello" -Path $dbPath } | Should -Not -Throw
+        }
+    }
+
     Context 'GetPsEdScript' {
         It 'Will throw if argument IndexName is empty string.' {
-            { Get-PsEdScript -IndexName "" -Path "example.db" } | Should -Throw
+            { Get-PsEdScript -IndexName "" -Path $dbPath } | Should -Throw
         }
         It 'Will return [string].' {
-            Get-PsEdScript -IndexName "abc" -Path "example.db" | Should -BeOfType [string]
+            Get-PsEdScript -IndexName "hello" -Path $dbPath | Should -BeOfType [string]
+        }
+        It 'Will return "world".' {
+            Get-PsEdScript -IndexName "hello" -Path $dbPath | Should -Be "world"
         }
     }
 }
