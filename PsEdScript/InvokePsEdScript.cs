e6a5f530ba76954672b698285823c8157a10d822
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-05 07:43:16 +0900

    add: Defined new PSCmdlet class "InvokePsEdScript".

---
+++ PsEdScript/InvokePsEdScript.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using LibEd.EdData;
+using System.Text;
+
+namespace PsEdScript;
+
+[Cmdlet(VerbsLifecycle.Invoke,"PsEdScript")]
+public class InvokePsEdScript : PSCmdlet
+{
+    [Parameter(
+        Mandatory = true
+        )]
+    public string IndexName { get; set; }
+
+    [Parameter(
+        Mandatory = false
+        )]
+    public string Path { get; set; }
+
+    [Parameter(
+        Mandatory = false
+        )]
+    public IEdDataLogicFactory EdDataLogicObject { get; set; }
+
+    protected IEdDataWorker Worker { get; set; }
+
+    protected override void BeginProcessing()
+    {
+        Worker = Common.GetEdDataWorker(SessionState, EdDataLogicObject, Path);
+        var plainBytes = Worker.Extract(IndexName);
+        var plainText = Encoding.UTF8.GetString(plainBytes);
+    }
+}
