e6040b001ed8e3f04c2d7bdb699f47fbd9158146
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-04 22:00:19 +0900

    add: Defined new PSCmdlet class "GetPsEdScript".

---
+++ PsEdScript/GetPsEdScript.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace PsEdScript;
+
+[Cmdlet(VerbsCommon.Get,"PsEdScript")]
+[OutputType(typeof(string))]
+public class GetPsEdScript : PSCmdlet
+{
+    [Parameter(
+        Mandatory = true
+        )]
+    public string IndexName { get; set; }
+
+    [Parameter(
+        Mandatory = false
+        )]
+    public string Path { get; set; }
+
+    protected override void BeginProcessing()
+    {
+        if(string.IsNullOrWhiteSpace(IndexName))
+        {
+            ThrowArgumentNullOrEmptyException(nameof(IndexName));
+        }
+
+        if(string.IsNullOrWhiteSpace(Path)) Path = Environment.GetEnvironmentVariable("PsEdScriptDatabasePath");
+        if(string.IsNullOrWhiteSpace(Path))
+        {
+            ThrowArgumentNullOrEmptyException(nameof(Path));
+        }
+
+        Console.WriteLine($"IndexName: {IndexName}, Path: {Path}");
+    }
+
+    protected virtual void ThrowArgumentNullOrEmptyException(string Name)
+    {
+        throw new ArgumentException($"The argument {Name} is null or empty.");
+    }
+}
