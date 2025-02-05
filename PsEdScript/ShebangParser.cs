d0c105b2841d692abb99871661eae5454579df81
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-06 01:37:12 +0900

    add: Implemented method "Parse" of class "ShebangParser".

--- PsEdScript/ShebangParser.cs
+++ PsEdScript/ShebangParser.cs
@@ -4,13 +4,32 @@ namespace PsEdScript.Parser;
 
 public class ShebangParser
 {
+    protected const string SHEBANG_PREFIX = "#!";
+    protected static readonly char[] SPLIT_DELIMITER_WHITESPACES = new char[] { ' ', '\t' };
+    protected static readonly char[] SPLIT_DELIMITER_PATH_SEPARATOR = new char[] { '/', '\\' };
     public bool IsShebang = false;
     public string Command;
     public string CommandExact;
     public static ShebangParser Parse(string text)
     {
-        ShebangParser result = new ShebangParser(false, "", "");
-        return result;
+        if (!text.StartsWith(SHEBANG_PREFIX))
+        {
+            return new ShebangParser(false, "", "");
+        }
+        string trimedCommand = text.Substring(SHEBANG_PREFIX.Length)
+            .TrimStart()
+            .TrimEnd();
+        string[] trimedWords = trimedCommand.Split(SPLIT_DELIMITER_WHITESPACES, StringSplitOptions.RemoveEmptyEntries);
+        if(trimedWords.Length == 2)
+        {
+            return new ShebangParser(true, trimedCommand, trimedWords[1]);
+        }
+        else if(trimedWords.Length == 1)
+        {
+            string[] pathItems = trimedCommand.Split(SPLIT_DELIMITER_PATH_SEPARATOR);
+            return new ShebangParser(true, trimedCommand, pathItems[pathItems.Length-1]);
+        }
+        return new ShebangParser(false, "", "");
     }
 
     protected ShebangParser(bool isShebang, string command, string commandExact)
