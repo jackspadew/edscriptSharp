d833d8440ebe9431f27b30aeb7f208d4fc856cab
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-06 02:09:03 +0900

    test: Added test "ParseExampleShebangWithCommandDefinedInEnv_ReturnCorrectValues".

--- PsEdScript.Tests/ShebangParser_Tests.cs
+++ PsEdScript.Tests/ShebangParser_Tests.cs
@@ -16,4 +16,16 @@ public class ShebangParser_Tests
         Assert.Equal("/usr/bin/pwsh", parsedInfo.Command);
         Assert.Equal("pwsh", parsedInfo.CommandExact);
     }
+
+    [Theory]
+    [InlineData("#! /usr/bin/env pwsh")]
+    [InlineData("#!    /usr/bin/env   pwsh   ")]
+    [InlineData("#! \t /usr/bin/env\tpwsh \t ")]
+    public void ParseExampleShebangWithCommandDefinedInEnv_ReturnCorrectValues(string firstLine)
+    {
+        var parsedInfo = ShebangParser.Parse(firstLine);
+        Assert.True(parsedInfo.IsShebang);
+        Assert.Equal("/usr/bin/env pwsh", parsedInfo.Command);
+        Assert.Equal("pwsh", parsedInfo.CommandExact);
+    }
 }
