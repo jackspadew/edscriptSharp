f6308b34301a2aaeb210e5dbad0c90f9b63c7fca
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-06 02:25:16 +0900

    test: Added test "ParseExampleShebangWithMultipleLines_ReturnCorrectValues".

--- PsEdScript.Tests/ShebangParser_Tests.cs
+++ PsEdScript.Tests/ShebangParser_Tests.cs
@@ -28,4 +28,17 @@ public class ShebangParser_Tests
         Assert.Equal("/usr/bin/env pwsh", parsedInfo.Command);
         Assert.Equal("pwsh", parsedInfo.CommandExact);
     }
+
+    [Theory]
+    [InlineData("#! /usr/bin/pwsh\r\ntext\r\ntext", "/usr/bin/pwsh")]
+    [InlineData("#! /usr/bin/pwsh\ntext\ntext", "/usr/bin/pwsh")]
+    [InlineData("#! /usr/bin/env pwsh\r\ntext\r\ntext", "/usr/bin/env pwsh")]
+    [InlineData("#! /usr/bin/env pwsh\ntext\ntext", "/usr/bin/env pwsh")]
+    public void ParseExampleShebangWithMultipleLines_ReturnCorrectValues(string firstLine, string actualCommand)
+    {
+        var parsedInfo = ShebangParser.Parse(firstLine);
+        Assert.True(parsedInfo.IsShebang);
+        Assert.Equal(actualCommand, parsedInfo.Command);
+        Assert.Equal("pwsh", parsedInfo.CommandExact);
+    }
 }
