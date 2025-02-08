6c30bc7d51d7193b4df705dfac56beec3bb2432c
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-09 07:11:44 +0900

    perf: Adjusted to increase some constant values used in encryption/decryption.
    This setting is still too fast when processing "Extract". However, it appears that "Stash" processing time is increasing exponentially. Therefore, it is in a situation where adjustments to increase the load further cannot be made.

--- LibEd/edData/IEdDataLogicFactory/implementation_EdDataLogicFactory.cs
+++ LibEd/edData/IEdDataLogicFactory/implementation_EdDataLogicFactory.cs
@@ -19,16 +19,16 @@ public class BasicEdDataLogicFactory : EdDataLogicFactoryBase, IEdDataLogicFacto
 
     protected override IEdDataCryptor DefaultCryptor => new EdDataCryptor(1000);
     protected override IEdDataCryptor InitialCryptor => new EdDataCryptor(10);
-    protected override IEdDataCryptor ChainZeroCryptor => new EdDataCryptor(10000);
+    protected override IEdDataCryptor ChainZeroCryptor => new EdDataCryptor(1000);
     protected override IEdDataCryptor MiddleWorkerCryptor => new EdDataCryptor(1000);
     protected override IEdDataCryptor LastWorkerCryptor => new EdDataCryptor(300);
     protected override IDatabaseOperator DefaultDatabaseOperator => new FakeInsertionDatabaseOperator(DbPath, true, 9999);
     protected override IDatabaseOperator LastWorkerDatabaseOperator => new EdDatabaseOperator(DbPath, true);
     protected override IEdDataHashCalculator DefaultHashCalculator => new EdDataHashCalculator(10000);
-   protected override IEdDataHashCalculator InitialWorkerHashCalculator => new EdDataHashCalculator(10);
-    protected override IEdDataHashCalculator ChainZeroWorkerHashCalculator => new EdDataHashCalculator(10000);
+    protected override IEdDataHashCalculator InitialWorkerHashCalculator => new EdDataHashCalculator(10);
+    protected override IEdDataHashCalculator ChainZeroWorkerHashCalculator => new EdDataHashCalculator(5000);
     protected override IEdDataHashCalculator MiddleChainWorkerHashCalculator => new EdDataHashCalculator(5000);
-    protected override IEdDataHashCalculator LastWorkerHashCalculator => new EdDataHashCalculator(5000);
+    protected override IEdDataHashCalculator LastWorkerHashCalculator => new EdDataHashCalculator(10000);
     protected override IMultipleKeyExchanger DefaultMultipleKeyExchanger => new DefaultMultipleKeyExchanger();
     protected override IMultipleKeyExchanger KeyBlendedMultipleKeyExchanger => new BasicKeyBlendedMultipleKeyExchanger();
     protected override IMultipleKeyExchanger ChainedMultipleKeyExchanger => new BasicExemplaryMultipleKeyExchanger();
