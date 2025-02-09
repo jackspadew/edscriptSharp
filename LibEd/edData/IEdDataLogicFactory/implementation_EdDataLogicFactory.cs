beb2efd9f42720b25182617bc46822e41fcc2aa5
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-10 08:53:53 +0900

    refactor: Refactored the constructor of class "BasicEdDataLogicFactory".
    Now use the value of a class property when instantiating a logic object.
    The default values for these properties are defined as in-class constants.
    Values defined as constants are adjusted for processing load and security considerations.
    Alternatively, the value provided in the constructor argument is used instead.
    
    This change was made to reduce the processing time of the test process.
    Therefore, there is no need to change the client code.

--- LibEd/edData/IEdDataLogicFactory/implementation_EdDataLogicFactory.cs
+++ LibEd/edData/IEdDataLogicFactory/implementation_EdDataLogicFactory.cs
@@ -4,31 +4,81 @@ namespace LibEd.EdData;
 
 public class BasicEdDataLogicFactory : EdDataLogicFactoryBase, IEdDataLogicFactory
 {
+    private const int MULTIPLE_ENCRYPT_COUNT_HEAVY = 1000;
+    private const int MULTIPLE_ENCRYPT_COUNT_NORMAL = 300;
+    private const int MULTIPLE_ENCRYPT_COUNT_LIGTH = 10;
+    private const int DBOPERATOR_FAKEINSERTION_COUNT_HEAVY = 998;
+    private const int DBOPERATOR_FAKEINSERTION_COUNT_NORMAL = 98;
+    private const int DBOPERATOR_FAKEINSERTION_COUNT_LIGTH = 10;
+    private const int HASH_STRETCHING_COUNT_HEAVY = 200000;
+    private const int HASH_STRETCHING_COUNT_NORMAL = 10000;
+    private const int HASH_STRETCHING_COUNT_LIGTH = 10;
+    private const int WORKERCHAIN_DEPTH_LIGTH = 1;
+    private const int WORKERCHAIN_DEPTH_HEAVY = 3;
+    protected int Default_Multiple_Encrypt_Count = MULTIPLE_ENCRYPT_COUNT_HEAVY;
+    protected int InitialWorker_Multiple_Encrypt_Count = MULTIPLE_ENCRYPT_COUNT_LIGTH;
+    protected int ChainZeroWorker_Multiple_Encrypt_Count = MULTIPLE_ENCRYPT_COUNT_HEAVY;
+    protected int MiddleWorker_Multiple_Encrypt_Count = MULTIPLE_ENCRYPT_COUNT_HEAVY;
+    protected int LastWorker_Multiple_Encrypt_Count = MULTIPLE_ENCRYPT_COUNT_NORMAL;
+    protected int Default_FakeInsertionCount = DBOPERATOR_FAKEINSERTION_COUNT_HEAVY;
+    protected int Default_Hash_Stretching_Count = HASH_STRETCHING_COUNT_HEAVY;
+    protected int InitialWorker_Hash_Stretching_Count = HASH_STRETCHING_COUNT_LIGTH;
+    protected int ChainZeroWorker_Hash_Stretching_Count = HASH_STRETCHING_COUNT_HEAVY;
+    protected int MiddleWorker_Hash_Stretching_Count = HASH_STRETCHING_COUNT_HEAVY;
+    protected int LastWorker_Hash_Stretching_Count = HASH_STRETCHING_COUNT_HEAVY;
     protected override string DbPath {get;set;}
     protected override byte[] Key {get;set;} = new byte[0];
 
-    public BasicEdDataLogicFactory(string dbPath, string password) : this(dbPath, password, 1)
-    {}
-    public BasicEdDataLogicFactory(string dbPath, string password, int depth)
+    public BasicEdDataLogicFactory(
+        string dbPath,
+        string password,
+        int default_MultipleEncryptCount = MULTIPLE_ENCRYPT_COUNT_HEAVY,
+        int initialWorker_MultipleEncryptCount = MULTIPLE_ENCRYPT_COUNT_LIGTH,
+        int chainZeroWorker_MultipleEncryptCount = MULTIPLE_ENCRYPT_COUNT_HEAVY,
+        int middleWorker_MultipleEncryptCount = MULTIPLE_ENCRYPT_COUNT_HEAVY,
+        int lastWorker_MultipleEncryptCount = MULTIPLE_ENCRYPT_COUNT_NORMAL,
+        int default_FakeInsertionCount = DBOPERATOR_FAKEINSERTION_COUNT_HEAVY,
+        int default_HashStretchingCount = HASH_STRETCHING_COUNT_HEAVY,
+        int initialWorker_HashStretchingCount = HASH_STRETCHING_COUNT_LIGTH,
+        int chainZeroWorker_HashStretchingCount = HASH_STRETCHING_COUNT_HEAVY,
+        int middleWorker_HashStretchingCount = HASH_STRETCHING_COUNT_HEAVY,
+        int lastWorker_HashStretchingCount = HASH_STRETCHING_COUNT_HEAVY,
+        int workerChainDepth = WORKERCHAIN_DEPTH_LIGTH
+        ) : this(dbPath, password)
     {
-        TargetWorkerChainDepth = depth;
+        this.Default_Multiple_Encrypt_Count = default_MultipleEncryptCount;
+        this.InitialWorker_Multiple_Encrypt_Count = initialWorker_MultipleEncryptCount;
+        this.ChainZeroWorker_Multiple_Encrypt_Count = chainZeroWorker_MultipleEncryptCount;
+        this.MiddleWorker_Multiple_Encrypt_Count = middleWorker_MultipleEncryptCount;
+        this.LastWorker_Multiple_Encrypt_Count = lastWorker_MultipleEncryptCount;
+        this.Default_FakeInsertionCount = default_FakeInsertionCount;
+        this.Default_Hash_Stretching_Count = default_HashStretchingCount;
+        this.InitialWorker_Hash_Stretching_Count = initialWorker_HashStretchingCount;
+        this.ChainZeroWorker_Hash_Stretching_Count = chainZeroWorker_HashStretchingCount;
+        this.MiddleWorker_Hash_Stretching_Count = middleWorker_HashStretchingCount;
+        this.LastWorker_Hash_Stretching_Count = lastWorker_HashStretchingCount;
+        this.TargetWorkerChainDepth = workerChainDepth;
+    }
+    public BasicEdDataLogicFactory(string dbPath, string password)
+    {
+        TargetWorkerChainDepth = WORKERCHAIN_DEPTH_LIGTH;
         DbPath = dbPath;
         Key = new byte[KeyBlendedMultipleKeyExchanger.Key.Length];
         SetPassword(password);
     }
 
-    protected override IEdDataCryptor DefaultCryptor => new EdDataCryptor(1000);
-    protected override IEdDataCryptor InitialCryptor => new EdDataCryptor(10);
-    protected override IEdDataCryptor ChainZeroCryptor => new EdDataCryptor(1000);
-    protected override IEdDataCryptor MiddleWorkerCryptor => new EdDataCryptor(1000);
-    protected override IEdDataCryptor LastWorkerCryptor => new EdDataCryptor(300);
-    protected override IDatabaseOperator DefaultDatabaseOperator => new FakeInsertionDatabaseOperator(DbPath, true, 9999);
+    protected override IEdDataCryptor DefaultCryptor => new EdDataCryptor(Default_Multiple_Encrypt_Count);
+    protected override IEdDataCryptor InitialCryptor => new EdDataCryptor(InitialWorker_Multiple_Encrypt_Count);
+    protected override IEdDataCryptor ChainZeroCryptor => new EdDataCryptor(ChainZeroWorker_Multiple_Encrypt_Count);
+    protected override IEdDataCryptor MiddleWorkerCryptor => new EdDataCryptor(MiddleWorker_Multiple_Encrypt_Count);
+    protected override IEdDataCryptor LastWorkerCryptor => new EdDataCryptor(LastWorker_Multiple_Encrypt_Count);
+    protected override IDatabaseOperator DefaultDatabaseOperator => new FakeInsertionDatabaseOperator(DbPath, true, Default_FakeInsertionCount);
     protected override IDatabaseOperator LastWorkerDatabaseOperator => new EdDatabaseOperator(DbPath, true);
-    protected override IEdDataHashCalculator DefaultHashCalculator => new EdDataHashCalculator(200000);
-    protected override IEdDataHashCalculator InitialWorkerHashCalculator => new EdDataHashCalculator(10);
-    protected override IEdDataHashCalculator ChainZeroWorkerHashCalculator => new EdDataHashCalculator(200000);
-    protected override IEdDataHashCalculator MiddleChainWorkerHashCalculator => new EdDataHashCalculator(200000);
-    protected override IEdDataHashCalculator LastWorkerHashCalculator => new EdDataHashCalculator(200000);
+    protected override IEdDataHashCalculator DefaultHashCalculator => new EdDataHashCalculator(Default_Hash_Stretching_Count);
+    protected override IEdDataHashCalculator InitialWorkerHashCalculator => new EdDataHashCalculator(InitialWorker_Hash_Stretching_Count);
+    protected override IEdDataHashCalculator ChainZeroWorkerHashCalculator => new EdDataHashCalculator(ChainZeroWorker_Hash_Stretching_Count);
+    protected override IEdDataHashCalculator MiddleChainWorkerHashCalculator => new EdDataHashCalculator(MiddleWorker_Hash_Stretching_Count);
+    protected override IEdDataHashCalculator LastWorkerHashCalculator => new EdDataHashCalculator(LastWorker_Hash_Stretching_Count);
     protected override IMultipleKeyExchanger DefaultMultipleKeyExchanger => new DefaultMultipleKeyExchanger();
     protected override IMultipleKeyExchanger KeyBlendedMultipleKeyExchanger => new BasicKeyBlendedMultipleKeyExchanger();
     protected override IMultipleKeyExchanger ChainedMultipleKeyExchanger => new BasicExemplaryMultipleKeyExchanger();
