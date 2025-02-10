a6cb5465cdddd8b5df5313eb9711fcdee6047ee7
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-10 10:21:23 +0900

    add: Defined IHashAlgorithmAdapter class "BLAKE3".

--- LibEd/HashAlgorithmAdapter/Adapter_BCHashAlgorithm.cs
+++ LibEd/HashAlgorithmAdapter/Adapter_BCHashAlgorithm.cs
@@ -23,3 +23,23 @@ public class SHA3 : BouncyCastleDigestAdaptorBase, IHashAlgorithmAdapter
         return new Sha3Digest(bitLength);
     }
 }
+
+public class BLAKE3 : BouncyCastleDigestAdaptorBase, IHashAlgorithmAdapter
+{
+    private int[] _legalBitLengthList = new int[]{ 224, 256, 384, 512 };
+    public override int[] LegalBitLengthList => _legalBitLengthList;
+
+    public BLAKE3()
+    {
+        this.BitLength = 512;
+    }
+    public BLAKE3(int bitLength)
+    {
+        this.BitLength = bitLength;
+    }
+
+    protected override IDigest CreateIDigestObj(int bitLength)
+    {
+        return new Blake3Digest(bitLength);
+    }
+}
