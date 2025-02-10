baab1474c1d9723125a4e012fcfc36ec75a16760
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-10 13:44:24 +0900

    test: Added the test target class "Skein","Whirlpool" to test class "IHashAlgorithmAdapter_CommonTests".

--- LibEd.Tests/HashAlgorithmAdapter/IHashAlgorithmAdapter_CommonTests.cs
+++ LibEd.Tests/HashAlgorithmAdapter/IHashAlgorithmAdapter_CommonTests.cs
@@ -14,6 +14,8 @@ public class IHashAlgorithmAdapter_CommonTests
         yield return new object[] { new HashAlgorithmAdapter.BouncyCastle.SHA3(), "BouncyCastle.SHA3" };
         yield return new object[] { new HashAlgorithmAdapter.BouncyCastle.BLAKE3(), "BouncyCastle.BLAKE3" };
         yield return new object[] { new HashAlgorithmAdapter.BouncyCastle.BLAKE2b(), "BouncyCastle.BLAKE2b" };
+        yield return new object[] { new HashAlgorithmAdapter.BouncyCastle.Skein(), "BouncyCastle.Skein" };
+        yield return new object[] { new HashAlgorithmAdapter.BouncyCastle.Whirlpool(), "BouncyCastle.Whirlpool" };
     }
 
     [Theory]
