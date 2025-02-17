1fca3e7553080bc35daf8df3f88237cd3347a106
jackspadew <201367434+jackspadew@users.noreply.github.com>
2025-02-17 23:38:22 +0900

    add: Implemented range check for argument values on the "NewPsEdScriptLogicObj" PSCmdlet class.

--- PsEdScript/NewPsEdScriptLogicObj.cs
+++ PsEdScript/NewPsEdScriptLogicObj.cs
@@ -1,5 +1,6 @@
 using System.Management.Automation;
 using LibEd.EdData;
+using System;
 
 namespace PsEdScript;
 
@@ -17,20 +18,58 @@ public class NewPsEdScriptLogicObj : PSCmdlet
         )]
     public string Password { get; set; }
 
+    private int _hashStretchingCount = Common.DEFAULT_HASH_STRETCHING_COUNT;
     [Parameter(
         Mandatory = false
         )]
-    public int HashStretchingCount { get; set; } = Common.DEFAULT_HASH_STRETCHING_COUNT;
+    public int HashStretchingCount {
+        get => _hashStretchingCount;
+        set {
+            int lowerLimit = 1;
+            if(value < lowerLimit)
+            {
+                ThrowExceptionBelowTheLowerLimit(nameof(HashStretchingCount), value, lowerLimit);
+            }
+            _hashStretchingCount = value;
+        }
+    }
 
+    private int _multipleEncryptionCount = Common.DEFAULT_MULTIPLE_ENCRYPTION_COUNT;
     [Parameter(
         Mandatory = false
         )]
-    public int MultipleEncryptionCount { get; set; } = Common.DEFAULT_MULTIPLE_ENCRYPTION_COUNT;
+    public int MultipleEncryptionCount {
+        get => _multipleEncryptionCount;
+        set {
+            int lowerLimit = 1;
+            if(value < lowerLimit)
+            {
+                ThrowExceptionBelowTheLowerLimit(nameof(MultipleEncryptionCount), value, lowerLimit);
+            }
+            _multipleEncryptionCount = value;
+        }
+    }
 
+    private int _fakeInsertionCount = Common.DEFAULT_FAKEINSERTION_COUNT;
     [Parameter(
         Mandatory = false
         )]
-    public int FakeInsertionCount { get; set; } = Common.DEFAULT_FAKEINSERTION_COUNT;
+    public int FakeInsertionCount {
+        get => _fakeInsertionCount;
+        set {
+            int lowerLimit = 0;
+            if(value < lowerLimit)
+            {
+                ThrowExceptionBelowTheLowerLimit(nameof(FakeInsertionCount), value, lowerLimit);
+            }
+            _fakeInsertionCount = value;
+        }
+    }
+
+    protected virtual ArgumentOutOfRangeException ThrowExceptionBelowTheLowerLimit(string name, int actual, int limit)
+    {
+        throw new ArgumentOutOfRangeException(name, $"An attempt was made to assign to \"{name}\" a value below {limit}. (value: {actual})");
+    }
 
     protected override void BeginProcessing()
     {
